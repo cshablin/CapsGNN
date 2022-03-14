0d1d9074b886ddb229c9ea22f0285a1294c1f880
Abdul Matin <muaz@Abduls-MacBook-Air.local>
2022-03-14 16:08:14 -0600

    adding all files

---
+++ src/regression_caps/main.py
@@ -0,0 +1,22 @@
+"""Running CapsGNN."""
+
+from utils import tab_printer
+from capsgnn import CapsGNNTrainer
+from param_parser import parameter_parser
+
+def main():
+    """
+    Parsing command line parameters, processing graphs, fitting a CapsGNN.
+    """
+    args = parameter_parser()
+
+    tab_printer(args)
+    model = CapsGNNTrainer(args)
+    #model.fit_classi()
+    model.fit()
+    #model.test_mse()
+    #model.score()
+    #model.save_predictions()
+
+if __name__ == "__main__":
+    main()
