1c8bee4ddde80db473a51b96b5ed884cfa75fb5b
cshablin <carmel@nimblebeauty.com>
2022-06-17 14:41:35 +0300

    notes for script input arguments used

--- src/classification/main.py
+++ src/classification/main.py
@@ -15,5 +15,8 @@ def main():
     model.score()
     model.save_predictions()
 
+
+# my input arguments for this script
+# --train-graph-folder ...\CapsGNN_fork\input\train_\ --test-graph-folder ...\CapsGNN_fork\input\test_\ --prediction-path ...\CapsGNN_fork\temp_output\watts_predictions.csv --epochs 50 --batch-size 128 --gcn-layers 4 --capsule-dimensions 8 --number-of-capsules 8 --lambd 0.5 --learning-rate 0.001 --weight-decay 0.0001
 if __name__ == "__main__":
     main()
